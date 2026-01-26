8e828791e0de23583ecd9d3d77b89d415aa0ed3e
Shubham kahar <Shubhamkahar196@gmail.com>
2026-01-26 14:08:23 +0530

    updated - HomePage

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,83 @@
+const Home = () => {
+  return (
+    <div className="relative min-h-screen">
+      {/* Background */}
+      <img
+        src="./background.png"
+        alt="background"
+        className="absolute inset-0 w-full h-full object-cover -z-10"
+      />
 
+      <main className="container mx-auto px-6">
+        {/* Navbar */}
+        <nav className="flex justify-between items-center py-6">
+          <h1 className="text-2xl font-bold text-white">
+            <span className="text-blue-400">Meet</span>
+             
+             <b>-</b>
+            <span className="text-red-400">Zone</span>
+          </h1>
 
-const Home = ()=>{
-    return(
-        <>
-        <div className="bg-black ">Home Page</div>
-        </>
-    )
-}
+          <div className="space-x-4">
+            <button className="border px-4 py-2 rounded text-white font-bold cursor-pointer">
+              Join as Guest
+            </button>
+            <button className="bg-yellow-500 text-white font-bold px-4 py-2 rounded cursor-pointer">
+              Login
+            </button>
+          </div>
+        </nav>
 
-export default Home
+        {/* Hero Section */}
+        <section className="flex flex-col md:flex-row items-center mt-16">
+          {/* Left */}
+          <div className="md:w-1/2 space-y-4">
+            <h1 className="text-5xl font-bold text-white">
+              <span className="text-yellow-500">Connect</span> with your
+            </h1>
+            <h2 className="text-5xl font-bold text-white">Loved Ones</h2>
+            <p className="text-white font-bold">
+              Cover a distance by meet-zone
+            </p>
+
+            <button className="bg-blue-500 text-white font-bold px-6 py-3 rounded cursor-pointer">
+              Get Started
+            </button>
+          </div>
+
+          {/* Right */}
+          <div className="md:w-1/2 mt-10 md:mt-0 flex justify-center">
+            <img
+              src="./mobile.png"
+              alt="mobile"
+              className="max-w-full md:w-3/4"
+            />
+          </div>
+        </section>
+      </main>
+      
+      {/* Footer */}
+<footer className="mt-20 border-t border-white/20 py-8">
+  <div className="container mx-auto px-6 flex flex-col md:flex-row justify-between items-center text-white">
+
+ 
+    <h2 className="text-xl font-bold mb-4 md:mb-0">
+      <span className="text-blue-400">Meet</span>-
+      <span className="text-red-400">Zone</span>
+    </h2>
+
+   
+
+    {/* Copyright */}
+    <p className="text-sm text-white/70">
+      © {new Date().getFullYear()} Meet-Zone. All rights reserved.
+    </p>
+  </div>
+</footer>
+
+    </div>
+    
+  );
+};
+
+export default Home;
